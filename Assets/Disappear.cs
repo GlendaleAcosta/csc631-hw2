c5fbbe2c6083fd19a6ded1e7f9f502e993adfdef
npng16 <npang098@gmail.com>
2021-02-23 10:57:58 -0800

    merged w/ Zack
    imported Zack's scene

---
+++ Assets/Disappear.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag.Equals("fishball"))
+        {
+
+            Destroy(other.gameObject);
+        }
+    }
+}
